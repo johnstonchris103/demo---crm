b0dc5b919963d27b41145ea2c7a6fed8354fec78
johnstonchris103 <johnston.chris103@gmail.com>
2026-02-12 11:49:20 -0500

    Create data-store.js

---
+++ netlify/functions/data-store.js
@@ -0,0 +1,20 @@
+let data = {
+  medications: {
+    "H2001": [
+      { medication: "Lisinopril", dosage: "20mg", refillsRemaining: 2 },
+      { medication: "Metformin", dosage: "500mg", refillsRemaining: 1 }
+    ],
+    "H2002": [
+      { medication: "Atorvastatin", dosage: "10mg", refillsRemaining: 3 }
+    ],
+    "H2003": [
+      { medication: "Albuterol", dosage: "2 puffs as needed", refillsRemaining: 0 }
+    ],
+    "H2004": [
+      { medication: "Levothyroxine", dosage: "75mcg", refillsRemaining: 2 },
+      { medication: "Amlodipine", dosage: "5mg", refillsRemaining: 4 }
+    ]
+  }
+};
+
+module.exports = data;
