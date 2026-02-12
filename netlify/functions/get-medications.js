03a2811255fc19408efb0cb653371604e52120d8
johnstonchris103 <johnston.chris103@gmail.com>
2026-02-12 13:09:55 -0500

    Update get-medications.js

--- netlify/functions/get-medications.js
+++ netlify/functions/get-medications.js
@@ -3,18 +3,23 @@ const data = require("./data-store");
 exports.handler = async function(event, context) {
 
   const memberId = event.queryStringParameters.memberId;
-
   const memberMeds = data.medications[memberId];
 
   if (!memberMeds) {
     return {
       statusCode: 404,
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({ error: "No medications found" })
     };
   }
 
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify(memberMeds)
   };
 };
