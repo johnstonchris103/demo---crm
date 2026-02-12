b487f31030534b9076537a78491134ab6e526d59
johnstonchris103 <johnston.chris103@gmail.com>
2026-02-12 11:49:50 -0500

    Update get-medications.js

--- netlify/functions/get-medications.js
+++ netlify/functions/get-medications.js
@@ -1,25 +1,10 @@
-exports.handler = async function(event, context) {
+const data = require("./data-store");
 
-  const medications = {
-    "H2001": [
-      { medication: "Lisinopril", dosage: "20mg", refillsRemaining: 2 },
-      { medication: "Metformin", dosage: "500mg", refillsRemaining: 1 }
-    ],
-    "H2002": [
-      { medication: "Atorvastatin", dosage: "10mg", refillsRemaining: 3 }
-    ],
-    "H2003": [
-      { medication: "Albuterol", dosage: "2 puffs as needed", refillsRemaining: 0 }
-    ],
-    "H2004": [
-      { medication: "Levothyroxine", dosage: "75mcg", refillsRemaining: 2 },
-      { medication: "Amlodipine", dosage: "5mg", refillsRemaining: 4 }
-    ]
-  };
+exports.handler = async function(event, context) {
 
   const memberId = event.queryStringParameters.memberId;
 
-  const memberMeds = medications[memberId];
+  const memberMeds = data.medications[memberId];
 
   if (!memberMeds) {
     return {
