19e3ee110cb1ab2e56c272d32f525614745e7698
johnstonchris103 <johnston.chris103@gmail.com>
2026-06-09 15:08:37 -0400

    Create get-available-appointments.js

---
+++ netlify/functions/get-available-appointments.js
@@ -0,0 +1,167 @@
+const availability = [
+  {
+    slotId: "S9001",
+    therapistId: "T2001",
+    therapistName: "Dr. Elena Rivera",
+    date: "2026-06-15",
+    time: "09:00",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9002",
+    therapistId: "T2001",
+    therapistName: "Dr. Elena Rivera",
+    date: "2026-06-15",
+    time: "13:30",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9003",
+    therapistId: "T2002",
+    therapistName: "Dr. Marcus Chen",
+    date: "2026-06-15",
+    time: "17:00",
+    durationMinutes: 50,
+    location: "Yonkers",
+    modality: "In-Person",
+    available: true
+  },
+  {
+    slotId: "S9004",
+    therapistId: "T2003",
+    therapistName: "Dr. Priya Patel",
+    date: "2026-06-16",
+    time: "08:30",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9005",
+    therapistId: "T2001",
+    therapistName: "Dr. Elena Rivera",
+    date: "2026-06-16",
+    time: "11:00",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9006",
+    therapistId: "T2002",
+    therapistName: "Dr. Marcus Chen",
+    date: "2026-06-16",
+    time: "15:30",
+    durationMinutes: 50,
+    location: "Yonkers",
+    modality: "In-Person",
+    available: true
+  },
+  {
+    slotId: "S9007",
+    therapistId: "T2003",
+    therapistName: "Dr. Priya Patel",
+    date: "2026-06-17",
+    time: "09:30",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9008",
+    therapistId: "T2001",
+    therapistName: "Dr. Elena Rivera",
+    date: "2026-06-17",
+    time: "14:00",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9009",
+    therapistId: "T2002",
+    therapistName: "Dr. Marcus Chen",
+    date: "2026-06-17",
+    time: "18:00",
+    durationMinutes: 50,
+    location: "Yonkers",
+    modality: "In-Person",
+    available: true
+  },
+  {
+    slotId: "S9010",
+    therapistId: "T2003",
+    therapistName: "Dr. Priya Patel",
+    date: "2026-06-18",
+    time: "10:00",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9011",
+    therapistId: "T2001",
+    therapistName: "Dr. Elena Rivera",
+    date: "2026-06-18",
+    time: "12:30",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9012",
+    therapistId: "T2002",
+    therapistName: "Dr. Marcus Chen",
+    date: "2026-06-18",
+    time: "16:30",
+    durationMinutes: 50,
+    location: "Yonkers",
+    modality: "In-Person",
+    available: true
+  },
+  {
+    slotId: "S9013",
+    therapistId: "T2003",
+    therapistName: "Dr. Priya Patel",
+    date: "2026-06-19",
+    time: "09:00",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9014",
+    therapistId: "T2001",
+    therapistName: "Dr. Elena Rivera",
+    date: "2026-06-19",
+    time: "15:00",
+    durationMinutes: 50,
+    location: "Bronx",
+    modality: "Telehealth",
+    available: true
+  },
+  {
+    slotId: "S9015",
+    therapistId: "T2002",
+    therapistName: "Dr. Marcus Chen",
+    date: "2026-06-19",
+    time: "19:00",
+    durationMinutes: 50,
+    location: "Yonkers",
+    modality: "In-Person",
+    available: true
+  }
+];
